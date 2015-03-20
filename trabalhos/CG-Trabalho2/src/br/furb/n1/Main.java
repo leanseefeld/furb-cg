ce8b3d9817bb2ebb61a034d0bbf6247e5f484228
GustavoSabel <gustavo.sabel.gs@gmail.com>
2015-03-20 20:00:42 -0300

    Ajustes no Exercício N1

--- trabalhos/CG-Trabalho2/src/br/furb/n1/Main.java
+++ trabalhos/CG-Trabalho2/src/br/furb/n1/Main.java
@@ -1,25 +1,12 @@
 package br.furb.n1;
 
-/// \file Exemplo_N2_Jogl_Eclipse.java
-/// \brief Exemplo_N2_Jogl_Eclipse: desenha uma linha na diagonal.
-/// \version $Revision: 1.0 $
-/// \author Dalton Reis.
-/// \date 03/05/13.
-/// Obs.: variaveis globais foram usadas por questoes didaticas mas nao sao recomendas para aplicacoes reais.
-
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.media.opengl.DebugGL;
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.awt.event.MouseMotionListener;
 
-public class Main implements GLEventListener, KeyListener, MouseListener,
-		MouseMotionListener {
+public class Main implements GLEventListener {
 	private float ortho2D_minX = -400.0f, ortho2D_maxX = 400.0f,
 			ortho2D_minY = -400.0f, ortho2D_maxY = 400.0f;
 	private GL gl;
@@ -40,11 +27,6 @@ public class Main implements GLEventListener, KeyListener, MouseListener,
 	}
 
 	public void SRU() {
-		// gl.glDisable(gl.GL_TEXTURE_2D);
-		// gl.glDisableClientState(gl.GL_TEXTURE_COORD_ARRAY);
-		// gl.glDisable(gl.GL_LIGHTING); //TODO: [D] FixMe: check if lighting
-		// and texture is enabled
-
 		// eixo x
 		gl.glColor3f(1.0f, 0.0f, 0.0f);
 		gl.glLineWidth(1.0f);
@@ -90,64 +72,14 @@ public class Main implements GLEventListener, KeyListener, MouseListener,
 		return (raio * Math.sin(Math.PI * angulo / 180.0));
 	}
 
-	public void keyPressed(KeyEvent e) {
-		System.out.println(" --- keyPressed ---");
-
-		System.out.println(" --- Redesenha ao sair do callback ---");
-		glDrawable.display();
+	@Override
+	public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {
+		
 	}
 
+	@Override
 	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
 			int arg4) {
-		System.out.println(" --- reshape ---");
-	}
-
-	public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {
-		System.out.println(" --- displayChanged ---");
-	}
-
-	public void keyReleased(KeyEvent arg0) {
-		System.out.println(" --- keyReleased ---");
-	}
-
-	public void keyTyped(KeyEvent arg0) {
-		System.out.println(" --- keyTyped ---");
-	}
-
-	public void mouseEntered(MouseEvent e) {
-	}
-
-	public void mouseExited(MouseEvent e) {
-	}
-
-	public void mousePressed(MouseEvent e) {
-		// if ((e.getModifiers() & e.BUTTON1_MASK) != 0) {
-		antigoX = e.getX();
-		antigoY = e.getY();
-		// }
-	}
-
-	public void mouseReleased(MouseEvent e) {
-	}
-
-	public void mouseClicked(MouseEvent e) {
-	}
-
-	public void mouseDragged(MouseEvent e) {
-		int movtoX = e.getX() - antigoX;
-		int movtoY = e.getY() - antigoY;
-		valorX += movtoX;
-		valorY -= movtoY;
-
-		// Dump ...
-		System.out.println("posMouse: " + movtoX + " / " + movtoY);
-
-		antigoX = e.getX();
-		antigoY = e.getY();
-
-		glDrawable.display();
-	}
-
-	public void mouseMoved(MouseEvent e) {
+		
 	}
 }
