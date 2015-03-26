6eb0c466b8bfd1b496c1526ebf6091862f853f9b
leanseefeld <leanseefeld@gmail.com>
2015-03-26 20:57:01 -0300

    Base de todos os exercícios

--- trabalhos/CG-Trabalho2/src/br/furb/n5/Main.java
+++ trabalhos/CG-Trabalho2/src/br/furb/n5/Main.java
@@ -1,7 +1,85 @@
 package br.furb.n5;
 
-public class Main {
-	public static void main(String[] args) {
+import javax.media.opengl.DebugGL;
+import javax.media.opengl.GL;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.glu.GLU;
+
+public class Main implements GLEventListener {
+	private float ortho2D_minX = -400.0f, ortho2D_maxX = 400.0f,
+			ortho2D_minY = -400.0f, ortho2D_maxY = 400.0f;
+	private GL gl;
+	private GLU glu;
+	private GLAutoDrawable glDrawable;
+	private double valorX = 200.0, valorY = 200.0;
+	private int antigoX, antigoY = 0;
+
+	public void init(GLAutoDrawable drawable) {
+		System.out.println(" --- init ---");
+		glDrawable = drawable;
+		gl = drawable.getGL();
+		glu = new GLU();
+		glDrawable.setGL(new DebugGL(gl));
+		System.out.println("Espa�o de desenho com tamanho: "
+				+ drawable.getWidth() + " x " + drawable.getHeight());
+		gl.glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	}
+
+	public void SRU() {
+		// eixo x
+		gl.glColor3f(1.0f, 0.0f, 0.0f);
+		gl.glLineWidth(1.0f);
+		gl.glBegin(GL.GL_LINES);
+		gl.glVertex2f(-200.0f, 0.0f);
+		gl.glVertex2f(200.0f, 0.0f);
+		gl.glEnd();
+		// eixo y
+		gl.glColor3f(0.0f, 1.0f, 0.0f);
+		gl.glBegin(GL.GL_LINES);
+		gl.glVertex2f(0.0f, -200.0f);
+		gl.glVertex2f(0.0f, 200.0f);
+		gl.glEnd();
+	}
+
+	// exibicaoPrincipal
+	public void display(GLAutoDrawable arg0) {
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		gl.glMatrixMode(GL.GL_PROJECTION);
+		gl.glLoadIdentity();
+		glu.gluOrtho2D(ortho2D_minX, ortho2D_maxX, ortho2D_minY, ortho2D_maxY);
+		gl.glMatrixMode(GL.GL_MODELVIEW);
+		gl.glLoadIdentity();
+
+		SRU();
+
+		gl.glColor3f(0.0f, 0.0f, 1.0f);
+		gl.glPointSize(3f);
+		gl.glBegin(GL.GL_POINTS);
+		for (int i = 0; i < 360; i += 5) {
+			gl.glVertex2d(RetornaX(i, 100), RetornaY(i, 100));
+		}
+		gl.glEnd();
+
+		gl.glFlush();
+	}
+
+	public double RetornaX(double angulo, double raio) {
+		return (raio * Math.cos(Math.PI * angulo / 180.0));
+	}
+
+	public double RetornaY(double angulo, double raio) {
+		return (raio * Math.sin(Math.PI * angulo / 180.0));
+	}
+
+	@Override
+	public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {
+		
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
+			int arg4) {
 		
 	}
 }
---
+++ trabalhos/CG-Trabalho2/src/br/furb/n6/Frame.java
@@ -0,0 +1,39 @@
+package br.furb.n6;
+
+import java.awt.BorderLayout;
+
+import javax.media.opengl.GLCanvas;
+import javax.media.opengl.GLCapabilities;
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class Frame extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private Main renderer = new Main();
+
+	private int janelaLargura = 400, janelaAltura = 400;
+
+	public Frame() {
+		super("Execício N1");
+		setBounds(300, 250, janelaLargura, janelaAltura + 22);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		getContentPane().setLayout(new BorderLayout());
+
+		GLCapabilities glCaps = new GLCapabilities();
+		glCaps.setRedBits(8);
+		glCaps.setBlueBits(8);
+		glCaps.setGreenBits(8);
+		glCaps.setAlphaBits(8);
+
+		GLCanvas canvas = new GLCanvas(glCaps);
+		add(canvas, BorderLayout.CENTER);
+		canvas.addGLEventListener(renderer);
+		canvas.requestFocus();
+	}
+
+	public static void main(String[] args) {
+		new Frame().setVisible(true);
+	}
+
+}
