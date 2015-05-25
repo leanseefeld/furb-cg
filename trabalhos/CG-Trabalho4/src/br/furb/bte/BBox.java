68161419338a2519b5458d80e11ef786254e6425
Gustavo Sabel <gustavo.sabel.gs@gmail.com>
2015-05-25 14:31:43 -0300

    Criado método de colisão
    Obs: O código não foi testado

--- trabalhos/CG-Trabalho4/src/br/furb/bte/BBox.java
+++ trabalhos/CG-Trabalho4/src/br/furb/bte/BBox.java
@@ -14,9 +14,27 @@ public class BBox {
     }
 
     public BBox(int maiorX, int menorX, int maiorY, int menorY) {
-	this.maior = new Ponto(maiorX, maiorY);
-	this.menor = new Ponto(menorX, menorY);
+		this.maior = new Ponto(maiorX, maiorY);
+		this.menor = new Ponto(menorX, menorY);
     }
+	
+	public BBox(List<Ponto> pontos) {
+		BBox bbox = new BBox();
+		bbox.setMaiorX(Integer.MIN_VALUE);
+		bbox.setMenorX(Integer.MAX_VALUE);
+		bbox.setMaiorY(Integer.MIN_VALUE);
+		bbox.setMenorY(Integer.MAX_VALUE);
+		for (Ponto ponto : pontos) {
+			if (ponto.X > bbox.getMaiorX())
+				bbox.setMaiorX(ponto.X);
+			if (ponto.X < bbox.getMenorX())
+				bbox.setMenorX(ponto.X);
+			if (ponto.Y > bbox.getMaiorY())
+				bbox.setMaiorY(ponto.Y);
+			if (ponto.Y < bbox.getMenorY())
+				bbox.setMenorY(ponto.Y);
+		}
+	}
 
     public int getMaiorX() {
 	return maior.X;
@@ -83,7 +101,20 @@ public class BBox {
     public Ponto getCentro() {
 	return new Ponto((this.getMaiorX() + this.getMenorX()) / 2, (this.getMaiorY() + this.getMenorY()) / 2);
     }
-
+	
+	public boolean estaColidindo(BBox bbox)
+	{
+		if( this.getMenorX() > bbox.getMaiorX() )
+			return false; 
+		if( this.getMaiorX < bbox.getMenorX )
+			return false; 
+		if( this.getMaiorY() > bbox.getMenorY() )
+			return false; 
+		if( this.getMenorY() < bbox.getMaiorY() )
+			return false; 
+		return true; 
+	}
+	
     /**
      * Verifica se a linha que forma entre os pontos A e B passa por dentro da BBOX
      * 
@@ -91,26 +122,8 @@ public class BBox {
      * @param pontoB
      */
     public boolean estaSendoCruzadoPor(Ponto pontoA, Ponto pontoB) {
-
-	List<Ponto> pontosVerificacao = new ArrayList<>();
-	pontosVerificacao.add(new Ponto(this.getMaiorX(), this.getMaiorY()));
-	pontosVerificacao.add(new Ponto(this.getMaiorX(), this.getMenorY()));
-	pontosVerificacao.add(new Ponto(this.getMenorX(), this.getMenorY()));
-	pontosVerificacao.add(new Ponto(this.getMenorX(), this.getMaiorY()));
-
-	Ponto pontoBBoxA = null;
-	Ponto pontoBBoxB = null;
-	for (int i = 0; i < pontosVerificacao.size(); i++) {
-	    pontoBBoxA = pontosVerificacao.get(i);
-	    if (i + 1 < pontosVerificacao.size())
-		pontoBBoxB = pontosVerificacao.get(i + 1);
-	    else
-		pontoBBoxB = pontosVerificacao.get(0);
-	    
-	    //TODO: E agora?
-
-	}
-
-	return false;
+		BBox bboxLinha = new BBox(Arras.asList(pontoA, pontoB));
+		boolean estaColidindo = this.estaColidindo(bboxLinha);
+		return false;
     }
 }
